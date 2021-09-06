2c6b8304e6d48bf330d29d48eadea3eab85829c1
Swati Arora <swarora@microsoft.com>
2021-09-06 20:59:09 +0530

    Shell script and manifest

---
+++ Notification.ps1
@@ -0,0 +1,260 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Bypass -SCope CurrentUser
+Clear-Host
+
+Import-Module -Name "./NotificationUtil.psm1" -Verbose
+
+# Function to Clean Up Post Script Run
+function CleanUp {
+    param (
+        $SpoContext
+    )
+
+    ##Dispose Sharepoint Online context
+    $SpoContext.Dispose()
+    
+    ##Remove Current Sessions
+    Remove-Sessions
+}
+
+# Function to Get User Choice
+function Get-UserChoice {
+    Write-Host ""
+    Write-Host This script is for communication with O365 group members within your company tenant. -ForegroundColor Yellow
+    Write-Host ""
+    Write-Host ========================================================================================================================
+    Write-Host "                                                  SCRIPT CONFIGURATION"
+    Write-Host ========================================================================================================================
+    Write-Host ""
+    Write-Host "Exchange Online User   :" $Configuration.'exo.user.name'
+    Write-Host "Notifying User         :" $Configuration.'teams.user.name'
+    Write-Host ========================================================================================================================
+    Write-Host ""
+    Write-Host "Press Y: to proceed N: to cancel and quit" -ForegroundColor Yellow
+
+    $Option=Read-Host "Enter your choice?"
+    Switch ($Option)
+    {
+	    Y {$Choice="Proceed"} 
+	    Default {$Choice="Quit"}
+    }
+
+    return $Choice
+}
+
+#Function to Send Notification to Group Memebers
+function Send-NotificationToGroupMembers {
+   param (
+        $SpoContext,
+        $GroupIds,
+        $Notifier,
+        $Notification,
+        $Config,
+        $LogID
+    )
+
+    $CurrentFolder = Get-Location
+
+    $SLog = "$($CurrentFolder)\Log-$($LogID)-NS.csv"
+    $FLog = "$($CurrentFolder)\Log-$($LogID)-NF.csv"
+
+    If(Test-Path $SLog) { Remove-Item $SLog}
+    If(Test-Path $FLog) { Remove-Item $FLog}
+
+    #Do for each EXO group as given in the configuration
+    ForEach ($GID in $GroupIds) 
+    {
+        $GID = $GID.Trim()
+        $Group = Get-UnifiedGroup –Identity $GID
+        Write-Host "Group Name   :" $Group.DisplayName 
+        Write-Host "Group Members:" $Group.GroupMemberCount 
+        Write-Host "Group Type   :" $Group.GroupType
+
+        $GroupMembers = Get-UnifiedGroupLinks –Identity $GID –LinkType Members | Select-Object @{Name="GroupName";Expression={$Group.DisplayName}},`
+        @{Name="UserName";Expression={$_.DisplayName}}, @{Name="Email";Expression={$_.PrimarySmtpAddress}}, @{Name="UserId";Expression={$_.ExternalDirectoryObjectId}}
+
+        #Do for each member of the group
+        ForEach ($Member in $GroupMembers) 
+        {
+            $User = $Member.UserId
+            if($Notifier -eq $User) { continue }
+
+            #Create teams one on one chat
+            $NewOne2OneChatResponse = New-One2OneChat -Header $Header -Sender $Notifier -Recepient $User
+            if($NewOne2OneChatResponse['StatusCode']) {
+                Publish-Error -SC $NewOne2OneChatResponse['StatusCode'] -Member $Member -Message "Unable to create one2one chat to user" -Log $FLog
+                continue 
+            }
+  
+            if ($NewOne2OneChatResponse.id)
+            {
+                #Send teams chat
+                $SendChatResponse = Send-Chat -Header $Header -ChatID $NewOne2OneChatResponse.id -Text $Notification
+               
+                if($SendChatResponse['StatusCode']) {
+                    Publish-Error -SC $SendChatResponse['StatusCode'] -Member $Member -Message "Unable to send one2one chat to user" -Log $FLog
+                    continue 
+                }
+            }
+
+            $Table = @{}
+            $Table.Add('GroupName', $member.GroupName)
+            $Table.Add('UserName', $member.UserName)
+            $Table.Add('Email', $member.Email)
+            $Table.Add('UserId', $User)
+            $Table.Add('ChatId', $NewOne2OneChatResponse.id)
+            $Table.Add('MsgId', $SendChatResponse.id)
+
+            $GroupMember = New-Object -TypeName PSObject -Property $Table
+
+            Publish-Success -Member $GroupMember -Message "Notification sent to user" -Log $SLog
+        }
+
+        Write-Host ""
+    }
+
+    If(Test-Path $SLog) { 
+        Send-SingleFileToSpo -SpoContext $SpoContext -DocLibraryName "TeamsNotification" -DestinationPath "Logs" -SourceFilePath $SLOG
+        Remove-Item $SLog
+    }
+
+    If(Test-Path $FLog) { 
+        Send-SingleFileToSpo -SpoContext $SpoContext -DocLibraryName "TeamsNotification" -DestinationPath "Logs" -SourceFilePath $FLOG
+        Remove-Item $FLog
+    }
+}
+
+#Function to Send Reminder to Group Memebers
+function Send-Reminder {
+   param (
+        $SpoContext,
+        $Notifier,
+        $Config,
+        $LogID,
+        $PreviewText
+    )
+
+    $CurrentFolder = Get-Location
+
+    $SLog = "$($CurrentFolder)\Log-$($LogID)-RS.csv"
+    $FLog = "$($CurrentFolder)\Log-$($LogID)-RF.csv"
+
+    If(Test-Path $SLog) { Remove-Item $SLog}
+    If(Test-Path $FLog) { Remove-Item $FLog}
+
+    $LogFileName = "Log-$($LogID)-NS.csv"
+    $LogFilePath = "$($CurrentFolder)\$($LogFileName)"
+
+    Get-SpoFile -SpoContext $SpoContext -SourceFileURL "$($SpoContext.Url)/TeamsNotification/$($LogFileName)" -TargetFilePath $LogFilePath
+
+    $NotificationObjects = Import-Csv "$(Get-Location)\$($LogFileName)" | ForEach-Object {
+        [PSCustomObject]@{
+            'GroupName' = $_.GroupName
+            'UserName' = $_.UserName
+            'Email' = $_.Email
+            'UserId' = $_.UserId
+            'ChatId' = $_.ChatId
+            'MsgId' = $_.MsgId
+        }
+    }
+
+    If(Test-Path $LogFilePath) { Remove-Item $LogFilePath}
+
+    #Do for each chat object 
+    ForEach ($Notification in $NotificationObjects) 
+    {
+        $User = $Notification.UserId
+        if($Notifier -eq $User) { continue }
+
+        if ($Notification.ChatId)
+        {
+            #Send teams chat activity feed
+            $SendChatActivityResponse = Send-ChatActivityFeed -Header $Header -Recepient $User -ChatID $Notification.ChatId `
+            -Messageid $Notification.MsgId -Preview $PreviewText -Activity "approvalRequired" `
+            -Params "{'name': 'deploymentId', 'value': '12345'}"
+
+            if($SendChatActivityResponse['StatusCode']) { 
+                Publish-Error -SC $SendChatActivityResponse['StatusCode'] -Member $Notification -Message "Unable to send reminder to user" -Log $FLog
+                continue 
+            }
+        }
+
+        Publish-Success -Member $Notification -Message "Reminder sent to user" -Log $SLog
+    }
+
+    Write-Host ""
+    If(Test-Path $SLog) { 
+        Send-SingleFileToSpo -SpoContext $SpoContext -DocLibraryName "TeamsNotification" -DestinationPath "Logs" -SourceFilePath $SLOG
+        Remove-Item $SLog
+    }
+
+    If(Test-Path $FLog) { 
+        Send-SingleFileToSpo -SpoContext $SpoContext -DocLibraryName "TeamsNotification" -DestinationPath "Logs" -SourceFilePath $FLOG
+        Remove-Item $FLog
+    }
+}
+
+#Get Configuration from system file
+$Configuration = Get-SystemFileContent -FileName ".\app.properties"
+
+##Get Sharepoint Online Context
+$SpoContext = Get-SpoContext -SpoSiteUrl $Configuration.'spo.site.url' `
+                    -SpoUserName $Configuration.'spo.user.name' `
+                    -SpoPassword $Configuration.'spo.user.password'
+
+#Get Configuration from Sharepoint Online
+#$RemoteConfiguration = Get-SpoFileContent -SpoContext $SpoContext -SourceFileURL "$($SpoContext.Url)/TeamsNotification/app2.properties"
+
+#Get Exchange Online and Teams Sessions
+Get-Sessions -ExoUserName $Configuration.'exo.user.name' -TeamsUserName $Configuration.'teams.user.name'
+
+#Get O365 user access token
+$Header = Get-UserAccessToken -id $Configuration.'app.id' -secret $Configuration.'app.secret' -Scope $Configuration.'app.scope' `
+-UserName $Configuration.'teams.user.name' -Password $Configuration.'teams.user.password' -Tenant $Configuration.'tenant.name'
+if($Header -eq $null) {
+    $ErrorMessage = "Error: Unable to obtain user access token"
+    Write-Host –ForegroundColor Red $ErrorMessage
+    return
+}
+
+$Choice=Get-UserChoice
+if($Choice -eq 'Proceed') {
+    
+    Write-Host "Proceeding..." -ForegroundColor Yellow
+    $NotificationList = Get-SpoListItems -SpoContext $SpoContext -ListTitle "Notification"
+
+    foreach($item in $NotificationList)
+    {
+        $Notification = @{
+            ID = ($item.FieldValues.ID)
+            Title = ($item.FieldValues.Title)
+            Notification = ($item.FieldValues.Notification)
+            ExoGroupIds = ($item.FieldValues.ExoGroupIds -split ',')
+            Send = ($item.FieldValues.Send)
+            Remind = ($item.FieldValues.Remind)
+            PreviewText = ($item.FieldValues.PreviewText)
+        }
+
+        if($Notification.Send) {
+
+            Write-Host "Executing Script to Send Notification Title $($Notification.Title)" -ForegroundColor Yellow
+
+            Send-NotificationToGroupMembers -SpoContext $SpoContext -Config $Configuration -LogID $Notification.ID `
+            -GroupIds $Notification.ExoGroupIds -Notifier $Configuration.'teams.user.id' -Notification $Notification.Notification
+        } 
+        elseif($Notification.Remind) {
+    
+            Write-Host "Executing Script to Send Reminder for Notification Title $($Notification.Title)" -ForegroundColor Yellow
+
+            Send-Reminder -SpoContext $SpoContext -Config $Configuration -LogID $Notification.ID `
+            -Notifier $Configuration.'teams.user.id' -PreviewText $Notification.PreviewText
+        }
+    }
+}
+elseif($Choice -eq 'Quit') {
+
+    Write-Host "Quitting..." -ForegroundColor Yellow
+}
+
+CleanUp -SpoContext $SpoContext
+
+
---
+++ NotificationUtil.psm1
@@ -0,0 +1,420 @@
+﻿# Add System.Web for urlencode
+Add-Type -AssemblyName System.Web
+
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client")
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.Runtime")
+
+#Module Imports
+Import-Module ExchangeOnlineManagement
+
+##############################################Exchange Online Utility Functions#############################################
+# Function to Connect Exchange Online
+function Connect-ExOnline {
+    param ($UserName)
+
+    Connect-ExchangeOnline -UserPrincipalName $UserName
+
+    #Check for connectivity
+    if ((Get-PSSession | Where-Object { $_.ConfigurationName -like "*Exchange*" }) -ne $null) {
+        Write-Host `Successfully connected to Exchange Online -ForegroundColor Yellow
+    }
+    else {
+        Write-Host `Not connected to Exchange Online -ForegroundColor Red
+    }
+}
+###############################################################################################################################
+
+
+##############################################Session Management Utility Functions#############################################
+# Function to Get Exo Teams Session
+function Get-Sessions {
+    param (
+        $ExoUserName,
+        $TeamsUserName
+    )
+
+    try {
+        Connect-ExOnline -UserName $ExoUserName
+        Connect-MicrosoftTeams -AccountId $TeamsUserName
+    
+    } catch {
+        write-host "Error: $($_.Exception.Message)" -Foregroundcolor Red
+    }
+}
+
+# Function To Remove existing sessions
+function Remove-Sessions() {
+
+    try {
+        Get-PSSession | Remove-PSSession
+        Write-Host All sessions in the current window has been removed. -ForegroundColor Yellow
+
+    } catch {
+        write-host "Error: $($_.Exception.Message)" -Foregroundcolor Red
+    }
+}
+###############################################################################################################################
+
+
+
+####################################################General Utility Functions##################################################
+
+# Function To get system file content
+function Get-SystemFileContent {
+    param (
+        $FileName
+    )
+
+    $Content = Get-Content $FileName -raw
+    $Content = $Content -replace "(\\r)?\\n", [Environment]::NewLine
+    $Content = ConvertFrom-StringData($Content)
+    return $Content
+}
+
+# Function To Publish Error
+function Publish-Error {
+    param (
+        $SC,
+        $Member,
+        $Message,
+        $Log
+    )
+
+    $ErrorMessage = "Error($($SC)): $($Message) $($Member.Email)"
+    Write-Host –ForegroundColor Red $ErrorMessage
+
+    $Member | Select-Object GroupName, UserName, Email | Export-CSV $Log -NoTypeInformation -Append
+}
+
+# Function To Publish Success
+function Publish-Success {
+    param (
+        $Member,
+        $Message,
+        $Log
+    )
+
+    $SuccessMessage = "Success: $($Message) $($Member.Email)"
+    Write-Host –ForegroundColor Green $SuccessMessage
+
+    if($Member.ChatId) {
+        $Member | Select-Object GroupName, UserName, Email, UserId, ChatId, MsgId | Export-CSV $Log -NoTypeInformation -Append
+    }
+    else {
+        $Member | Select-Object GroupName, UserName, Email | Export-CSV $Log -NoTypeInformation -Append
+    }
+}
+###############################################################################################################################
+
+
+####################################################MS Teams Utility Functions#################################################
+# Function to Get Authentication Token
+function Get-UserAccessToken {
+    param (
+        $ID,
+        $Secret,
+        $Scope,
+        $UserName,
+        $Password,
+        $Tenant
+    )
+    
+    # Authentication url
+    $URL = "https://login.microsoftonline.com/" + $Tenant + "/oauth2/v2.0/token"
+
+    # Create body
+    $Body = @{
+        client_id = $ID
+	    client_secret = $Secret
+	    scope = $Scope
+	    grant_type = 'password'
+        userName = $UserName
+        password = $Password
+    }
+
+    # Splat the parameters for Invoke-Restmethod for cleaner code
+    $Splat = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Method = 'POST'
+        Body = $Body
+        Uri = $URL
+    }
+
+    try {
+        # Request the token!
+        $Response = Invoke-RestMethod @splat
+
+        # Create header
+        $Header = @{
+            Authorization = "$($Response.token_type) $($Response.access_token)"
+        }
+    } catch {
+        $Header = $null
+    }
+
+    return $Header
+}
+
+# Function to Create New One2One Teams Chat
+function New-One2OneChat {
+    param (
+        $Sender,
+        $Recepient,
+        $Header
+    )
+
+    # One on one chat creation url
+    $URL = "https://graph.microsoft.com/beta/chats"
+
+    # Create body
+    $Body = '
+    {
+        "chatType": "oneOnOne",
+        "members": [
+            {
+                "@odata.type": "#microsoft.graph.aadUserConversationMember",
+                "roles": ["owner"],
+                "user@odata.bind": "https://graph.microsoft.com/beta/users('+"'$Sender'"+')"
+            },
+            {
+                "@odata.type": "#microsoft.graph.aadUserConversationMember",
+                "roles": ["owner"],
+                "user@odata.bind": "https://graph.microsoft.com/beta/users('+"'$Recepient'"+')"
+            }
+        ]
+    }'
+
+    try {
+        # Invoke REST api to post chat
+        $Response = Invoke-RestMethod -Uri $URL -Headers $Header -Body $Body -Method Post -ContentType "application/json"
+    } catch {
+        $Response = @{
+            StatusCode = $_.Exception.Response.StatusCode.value__;
+            StatusDescription = $_.Exception.Response.StatusDescription
+        }
+    }
+
+    $Response
+}
+
+# Function to Send One2One Teams Chat
+function Send-Chat {
+    param (
+        $ChatID,
+        $Text,
+        $Header
+    )
+    
+    # Chat url
+    $URL = "https://graph.microsoft.com/beta/chats/$($ChatID)/messages"
+
+    # Create body
+    $Body = "
+    {
+      'body': {
+        'content': '$($Text)'
+      }
+    }"
+
+    try {
+        # Invoke REST api to post chat
+        $Response = Invoke-RestMethod -Uri $URL -Headers $Header -Body $Body -Method Post -ContentType "application/json"
+    } catch {       
+        $Response = @{
+            StatusCode = $_.Exception.Response.StatusCode.value__;
+            StatusDescription = $_.Exception.Response.StatusDescription
+        }
+    }
+
+    return $Response
+}
+
+# Function to Send Reminder
+function Send-ChatActivityFeed {
+    param (
+        $Recepient,
+        $ChatID,
+        $Messageid,
+        $Activity,
+        $Preview,
+        $Params,
+        $Header
+    )
+    
+    # Chat activity feed url
+    $URL = "https://graph.microsoft.com/beta/chats/$($ChatID)/sendActivityNotification"
+
+    # Create body
+    $Body ="{
+        'topic': {
+            'source': 'entityUrl',
+            'value': 'https://graph.microsoft.com/beta/chats/$($ChatID)/messages/$($Messageid)'
+        },
+        'activityType': '$($Activity)',
+        'previewText': {
+            'content': '$($Preview)'
+        },
+         'recipient': {
+            '@odata.type': 'microsoft.graph.aadUserNotificationRecipient',
+            'userId': '$($Recepient)'
+        },
+        'templateParameters': [$($Params)] 
+    }"
+
+    try {
+        # Invoke REST api to post chat activity feed
+        $Response = Invoke-RestMethod -Uri $URL -Headers $Header -Body $Body -Method Post -ContentType "application/json"
+    } catch {
+        $Response = @{
+            StatusCode = $_.Exception.Response.StatusCode.value__;
+            StatusDescription = $_.Exception.Response.StatusDescription
+        }
+    }
+
+    return $Response
+}
+###############################################################################################################################
+
+
+###########################################Sharepoint Online Utility Functions#################################################
+
+# Function to Get Sharepoint Online Context
+function Get-SpoContext {
+    param (
+        $SpoSiteUrl,
+        $SpoUserName,
+        $SpoPassword
+    )
+    
+    try {
+        $SecurePassword = $SpoPassword | ConvertTo-SecureString -AsPlainText -Force
+        $Context = New-Object Microsoft.SharePoint.Client.ClientContext($SpoSiteUrl)
+        $Context.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($SpoUserName, $SecurePassword)
+        return $Context
+
+    } catch {
+        write-host "Error: $($_.Exception.Message)" -Foregroundcolor Red
+    }
+}
+
+# Function to Download Sharepoint Online File
+function Get-SpoFile {
+    param (
+        $SpoContext,
+        $SourceFileURL,
+        $TargetFilePath
+    )
+       
+    try {
+
+        $SourceFileName = Split-path $SourceFileURL -leaf
+
+        $SourceFile = $SpoContext.web.GetFileByUrl($SourceFileURL)
+        $SpoContext.Load($SourceFile)
+        $SpoContext.ExecuteQuery()
+
+        [Microsoft.SharePoint.Client.FileInformation] $SourceFileInfo = [Microsoft.SharePoint.Client.File]::OpenBinaryDirect($SpoContext,$SourceFile.ServerRelativeUrl);
+        [System.IO.FileStream] $TargetFileStream = [System.IO.File]::Open($TargetFilePath,[System.IO.FileMode]::Create);   
+        $SourceFileInfo.Stream.CopyTo($TargetFileStream);
+        $TargetFileStream.Close();
+
+    } catch {
+        write-host "Error: $($_.Exception.Message)" -Foregroundcolor Red
+    }
+}
+
+# Function to Get Sharepoint Online File Content
+function Get-SpoFileContent {
+    param (
+        $SpoContext,
+        $SourceFileURL
+    )
+       
+    try {
+
+        $SourceFileName = Split-path $SourceFileURL -leaf
+
+        $SourceFile = $SpoContext.web.GetFileByUrl($SourceFileURL)
+        $SpoContext.Load($SourceFile)
+        $SpoContext.ExecuteQuery()
+
+        $TargetFilePath = "$(Get-Location)\$($SourceFileName)"
+
+        [Microsoft.SharePoint.Client.FileInformation] $SourceFileInfo = [Microsoft.SharePoint.Client.File]::OpenBinaryDirect($SpoContext,$SourceFile.ServerRelativeUrl);
+        [System.IO.FileStream] $TargetFileStream = [System.IO.File]::Open($TargetFilePath,[System.IO.FileMode]::Create);   
+        $SourceFileInfo.Stream.CopyTo($TargetFileStream);
+        $TargetFileStream.Close();
+
+        $Content = Get-Content $SourceFileName -raw
+        $Content = $Content -replace "(\\r)?\\n", [Environment]::NewLine
+        $Content = ConvertFrom-StringData($Content)
+
+        If(Test-Path $TargetFilePath) { Remove-Item $TargetFilePath}
+
+        return $Content
+
+    } catch {
+        write-host "Error: $($_.Exception.Message)" -Foregroundcolor Red
+    }
+}
+
+# Function to save single file to Sharepoint Online document library
+function Send-SingleFileToSpo {
+    param (
+        $SpoContext,
+        $DocLibraryName,
+        $DestinationPath,
+        $SourceFilePath
+    )
+
+    try {
+        #Get the Library
+        $DocLibrary =  $SpoContext.Web.Lists.GetByTitle($DocLibraryName)
+ 
+        #Get file from disk
+        $FileStream = ([System.IO.FileInfo] (Get-Item $SourceFilePath)).OpenRead()
+
+        #Get source file Name from source file path
+        $SourceFileName = Split-path $SourceFilePath -leaf
+   
+        #Upload single file to sharepoint online document library
+        $FileCreationInfo = New-Object Microsoft.SharePoint.Client.FileCreationInformation
+        $FileCreationInfo.Overwrite = $true
+        $FileCreationInfo.ContentStream = $FileStream
+        $FileCreationInfo.URL = $SourceFileName
+        $FileUploaded = $DocLibrary.RootFolder.Files.Add($FileCreationInfo)
+  
+        $SpoContext.Load($FileUploaded)
+        $SpoContext.ExecuteQuery()
+ 
+        #Close source file stream
+        $FileStream.Close()
+
+    } catch {
+        write-host "Error: $($_.Exception.Message)" -Foregroundcolor Red
+    }
+}
+
+# Function to Get List Items By List Title
+function Get-SpoListItems {
+   param (
+        [Microsoft.SharePoint.Client.ClientContext]$SpoContext,
+        [String]$ListTitle
+    )
+
+    try {
+        $List = $SpoContext.Web.Lists.GetByTitle($ListTitle)
+        $Query = [Microsoft.SharePoint.Client.CamlQuery]::CreateAllItemsQuery()
+        $ListItems = $List.GetItems($Query)
+        $SpoContext.Load($ListItems)
+        $SpoContext.ExecuteQuery()
+        return $ListItems 
+
+    } catch {
+        write-host "Error: $($_.Exception.Message)" -Foregroundcolor Red
+    }
+}
+
+###############################################################################################################################
+
+
